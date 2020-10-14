6907f7bd84f45276ebb4fdaeaf7fdb8229e42da6
zxiro <a0963212510@gmail.com>
2020-10-14 22:00:44 +0800

    Modified get_usa_data, parser and build_train_data into MLB requirement

--- get_usa_data.py
+++ get_usa_data.py
@@ -3,18 +3,16 @@ import datetime as dt
 import pandas as pd
 import yfinance as yf
 from pandas_datareader import data as web
-from build_config import index_dic
-from build_config import stock_dic
-
+from daetime import date, timedelta
 def get_usa_index():
     yf.pdr_override()
-    usa_dict = index_dic['index']
-    features = index_dic['features']
-    start = index_dic['date']
-    end = index_dic['end_date']
+    usa_dict = 'sp'
+    features = ['Close', 'Open', 'High', 'Low', 'Volume']
+    start = date.today()-timedelta(days=2)
+    end = date.today()
     index_list = []
     for key in usa_dict:
-        df = pd.DataFrame(web.get_data_yahoo(key, start, end))
+        df = pd.DataFrame(web.get_data_yahoo(key, start, end)) #get the S&P index in past three days
         df = df[df.columns[df.columns.isin(features)]]
         df.rename(columns = { "Open":usa_dict[key]+"_open", "High":usa_dict[key]+"_high", "Low":usa_dict[key]+"_low", "Close":usa_dict[key]+"_close", "Adj Close":usa_dict[key]+"_adj_close"}, inplace = True)
         index_list.append(df)
