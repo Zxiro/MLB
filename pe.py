999dfeb78850b5100d21684ccec9d0e2a31b7278
zxiro <a0963212510@gmail.com>
2021-01-05 01:52:23 +0800

    Fix the daily pe crawler

--- pe.py
+++ pe.py
@@ -1,54 +1,42 @@
-import requests as rq
-from bs4 import BeautifulSoup
-#import beautifulsoup4
-import numpy as np
 import csv
 import datetime
 import os
 import time
 import json
+import requests as rq
+import numpy as np
+from bs4 import BeautifulSoup
 
 def crawl_pe(date):
     datetime = str(date).split(' ')[0].replace('-','')
     link="https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=html&date="+datetime +"&selectType=ALL"
-    print(link)
     r = rq.get(link)
     soup = BeautifulSoup(r.text, "lxml")
+    a = soup.find('tr').find_next_siblings()
+    t = a[0].find_all('td')
+    pe_pos = 0
+    for i in t:
+        if(i.text == '本益比'):
+            break
+        pe_pos+=1
     a_tags = soup.find('tbody')
-    print(a_tags)
+
     if(a_tags == None):
-        return a_tags
+        return a_tags 
+
     a_tags = a_tags.find_all('tr')
-    i=2
-    #print(a_tags)
-    lis=[]
-    tmp = 0
     pe = {}
-    stock_num = 0
-    stock_pe = 0
-    for tag in a_tags:
-        for x in tag:
-            if(tmp == 1):
-                print(x.string)
-                stock_num = x.string
-            if(tmp == 11):
-                #print(x)
-                print(x.string)
-                stock_pe = x.string
-            tmp = tmp + 1
-        #exit()
-        pe[stock_num] = stock_pe
-        tmp = 0
-        #print(tag)
-    #print(pe)
-    #with open('test.csv', 'w') as f:
-    #    for key in pe.keys():
-    #            f.write("%s,%s\n"%(key,pe[key]))
-    #
-    if not os.path.exists('../pe_data/'):
+    for at in a_tags:
+        t = at.find_all('td')# list of tr eles
+        print(t)
+        pe[t[0].text] = t[pe_pos].text
+    print(pe)
+    exit()
+
+    if not os.path.exists('./pe_data/'):
         print("x")
         os.makedirs('./pe_data/')
-    json.dump(pe, open(f'./pe_data/{datetime}.json', 'w'), indent=4)
+    #json.dump(pe, open(f'./pe_data/{datetime}.json', 'w'), indent=4)
 
 
 y=2020
@@ -56,7 +44,7 @@ m=12
 d=25
 n_days = 3000
 date = datetime.datetime.now()
-date = datetime.date(2017, 4, 13)
+date = datetime.date(2014, 7, 17)
 fail_count = 0
 allow_continuous_fail_count = 5
 data = {}
